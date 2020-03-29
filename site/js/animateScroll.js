f164a92cac13b5f813f62d9fc8d44886ffa44551
Diego Alvarez <diegoalvarez.cursos@gmail.com>
2020-03-29 13:21:16 -0400

    Animaciones de Scroll

---
+++ site/js/animateScroll.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  $('#homeButton').click(function(){
+
+    $('#home').animatescroll({scrollSpeed:2000,easing:'easeInOutBack'});
+  });
+
+  $('#aboutButton').click(function(){
+
+    $('#about').animatescroll({scrollSpeed:2000,easing:'easeInOutBack'});
+  });
+
+  $('#portfolioButton').click(function(){
+
+    $('#portfolio').animatescroll({scrollSpeed:2000,easing:'easeInOutBack'});
+  });
+
+  $('#contactButton').click(function(){
+
+    $('#contact').animatescroll({scrollSpeed:2000,easing:'easeInOutBack'});
+  });
+
+});
